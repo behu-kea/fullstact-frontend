e0fe4a16351d7fe78d0be8d6f89ef1491c0369df
Benjamin Hughes <behu@kea.dk>
2021-10-25 13:41:05 +0200

    asd

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ import renderUser from "./pages/user/user.js";
 import renderLogin from "./pages/login/login.js";
 
 // "http://localhost:5552/api/hello"
-window.apiUrl = "https://tomas-order-site.herokuapp.com";
 
 const githubRepoName = "/fullstact-frontend";
 const isLocalhost =
   window.location.host.indexOf("127.0.0.1") != -1 ||
   window.location.host.indexOf("localhost") != -1;
+
+const localApiUrl = "http://localhost:5552/api/hello";
+const prodApiUrl = "https://tomas-order-site.herokuapp.com";
+
+window.apiUrl = isLocalhost ? localApiUrl : prodApiUrl;
 const root = isLocalhost ? "/" : githubRepoName;
 window.router = new Navigo(root, { hash: true });
 
