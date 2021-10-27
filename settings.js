c8c718421817fc83ba17fe582b0e16a9e7a2fd82
Benjamin Hughes <behu@kea.dk>
2021-10-27 11:12:43 +0200

    asd

--- settings.js
+++ settings.js
@@ -1,5 +1,4 @@
 export default function () {
-  const githubRepoName = "fullstact-frontend";
   const isLocalhost =
     window.location.host.indexOf("127.0.0.1") != -1 ||
     window.location.host.indexOf("localhost") != -1;
@@ -10,9 +9,6 @@ export default function () {
   // when fetching data from the api we need to know what the url is
   // It is different if you are developing locally or have the site deployed
   window.apiUrl = isLocalhost ? localApiUrl : prodApiUrl;
-
-  // The root is for the navigo router. It need to know the root route
-  window.root = isLocalhost ? "/" : githubRepoName;
 
   if (isLocalhost) {
     const div = document.createElement("div");
